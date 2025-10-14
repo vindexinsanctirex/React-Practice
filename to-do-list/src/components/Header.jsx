b2a16a27e1a90ccba02ae46b0f2249b47fefd1f1
Caio S. Duarte <caiosduarte@protonmail.com>
2025-10-14 19:26:54 +0000

    att

--- to-do-list/src/components/Header.jsx
+++ to-do-list/src/components/Header.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function Header() {
+  useEffect(() => {
+    const header = document.querySelector('header');
+    
+    const handleMouseMove = (e) => {
+      const rect = header.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      
+      header.style.setProperty('--mouse-x', `${x}px`);
+      header.style.setProperty('--mouse-y', `${y}px`);
+    };
+
+    header.addEventListener('mousemove', handleMouseMove);
+    
+    return () => {
+      header.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
   return (
     <header>
-      <h1>Lista de Tarefas</h1>
+      <h1 style={{fontFamily: 'Fantasy, Papyrus, "Times New Roman"'}}>Lista de Tarefas</h1>
     </header>
   );
 }
